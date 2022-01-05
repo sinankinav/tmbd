733b7ac0cbf79ebd4a7c4b0a06f61f897a1862c3
sinankinav <sinankinav8@gmail.com>
2022-01-05 23:54:14 +0300

    original
    original

---
+++ src/components/App.js
@@ -0,0 +1,93 @@
+import React from "react";
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import axios from "axios";
+
+
+class App  extends React.Component {
+state = { movies :  [],
+ 
+    searchQuery:""
+}
+
+// async componentDidMount(){
+//     const baseURL = "http://localhost:3002/movies";
+//     const response = await fetch(baseURL);
+//  console.log(response)
+//     const data =await response.json();
+// console.log(data)
+//     this.setState({movies:data})
+// }
+
+async componentDidMount() {
+  const response = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=1829ed0b31941257c9344bbea724ceea&language=en-US&page=1");
+  console.log(response);
+  this.setState({movies:response.data.results})
+}
+
+// deleteMovie =(movie)=> {
+//   const newMovieList =this.state.movies.filter(
+//     m =>m.id !== movie.id
+//   );
+
+// // this.setState({
+// //   movies: newMovieList
+// // })
+// this.setState(state => ({
+//   movies: newMovieList
+// }))
+// }
+
+//FETCH API
+// deleteMovie =async (movie)=> {
+//   const baseURL = `http://localhost:3002/movies/${movie.id};`
+//   await fetch(baseURL,{
+//       method:"DELETE"
+//   })
+//   const newMovieList =this.state.movies.filter(
+//     m =>m.id !== movie.id
+//   );
+
+// this.setState(state => ({
+//   movies: newMovieList
+// }))
+// }
+
+//AXIOS API
+deleteMovie =async (movie)=> {
+  axios.delete = ( `http://localhost:3002/movies/${movie.id};`)
+  
+  const newMovieList =this.state.movies.filter(
+    m =>m.id !== movie.id
+  );
+
+this.setState(state => ({
+  movies: newMovieList
+}))
+}
+
+
+searchMovie = (event) => {
+  // console.log(event.target.value)
+  this.setState({searchQuery:event.target.value})
+}
+
+   render () { 
+     let filteredMovies = this.state.movies.filter(
+       (movie) =>{
+         return movie.title.toLowerCase().indexOf(this.state.searchQuery)!== -1
+       }
+     )
+     return (
+    <div className="container">
+        <div className="row">
+            <div className="col-lg-12">
+                <SearchBar searchMovieProp={this.searchMovie}/>
+           </div>
+        </div>
+        <MovieList movies={filteredMovies} deleteMovieProp={this.deleteMovie}/>
+    </div>
+)} 
+
+}
+export default App;
